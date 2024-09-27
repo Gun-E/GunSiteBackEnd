b97104c7aebdd89e5d367324c41ca30fbf795ad2
gun <rkdrjs71@naver.com>
2024-09-27 16:39:58 +0900

    feat : 로그인 구현

--- src/main/java/site/gun/springserver/user/exception/UserAlreadyExist.java
+++ src/main/java/site/gun/springserver/user/exception/UserAlreadyExist.java
@@ -2,6 +2,6 @@ package site.gun.springserver.user.exception;
 
 public class UserAlreadyExist extends RuntimeException{
     public UserAlreadyExist() {
-        super("이미 존재하는 유저 입니다");
+        super("이미 존재 하는 유저 입니다");
     }
 }
