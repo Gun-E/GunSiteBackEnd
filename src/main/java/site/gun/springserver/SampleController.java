317ea6e82407ae992545f246f0c3f704558a11b0
강건 <rkdrjs71@naver.com>
2025-01-30 00:05:20 +0900

    fix : 포트오류 수정

---
+++ src/main/java/site/gun/springserver/SampleController.java
@@ -0,0 +1,12 @@
+package site.gun.springserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
